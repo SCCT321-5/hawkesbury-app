91039c974ec58149bdacfc73e9ea0323a173ff32
abdulrhamn20 <66347752+abdulrhamn20@users.noreply.github.com>
2021-12-06 16:02:14 +1100

    Add files via upload

---
+++ eventarray.js
@@ -0,0 +1,138 @@
+const  eventarray = [
+    {
+      id: "1",
+      name: "Summer concert",
+      address: "9/62 Argyle Street, South Windsor NSW 2756",
+      category: "Concerts",
+      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque est metus, gravida vel ex volutpat, posuere euismod tortor. Pellentesque tincidunt, mi ac varius blandit, quam orci dignissim risus, vitae rutrum orci urna ut neque.
+     Pellentesque condimentum ut libero id blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.`,
+     eventtime: "7PM",
+     eventdate: "5/12/2021",
+      image:
+        "http://www.showticksecure.com/DBTR/VenueNet/_uploads/event_images/1_lrg_20211705131414_img.jpg",
+    },
+    {
+        id: "2",
+        name: "Summer concert",
+        address: "9/62 Argyle Street, South Windsor NSW 2756",
+        category: "Concerts",
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque est metus, gravida vel ex volutpat, posuere euismod tortor. Pellentesque tincidunt, mi ac varius blandit, quam orci dignissim risus, vitae rutrum orci urna ut neque.
+       Pellentesque condimentum ut libero id blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.`,
+       eventtime: "7PM",
+       eventdate: "5/12/2021",
+        image:
+          "http://www.showticksecure.com/DBTR/VenueNet/_uploads/event_images/1_lrg_20211705131414_img.jpg",
+      },
+      {
+        id: "3",
+        name: "Summer concert",
+        address: "9/62 Argyle Street, South Windsor NSW 2756",
+        category: "Concerts",
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque est metus, gravida vel ex volutpat, posuere euismod tortor. Pellentesque tincidunt, mi ac varius blandit, quam orci dignissim risus, vitae rutrum orci urna ut neque.
+       Pellentesque condimentum ut libero id blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.`,
+       eventtime: "7PM",
+       eventdate: "5/12/2021",
+        image:
+          "http://www.showticksecure.com/DBTR/VenueNet/_uploads/event_images/1_lrg_20211705131414_img.jpg",
+      },
+      {
+        id: "4",
+        name: "Summer concert",
+        address: "9/62 Argyle Street, South Windsor NSW 2756",
+        category: "Concerts",
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque est metus, gravida vel ex volutpat, posuere euismod tortor. Pellentesque tincidunt, mi ac varius blandit, quam orci dignissim risus, vitae rutrum orci urna ut neque.
+       Pellentesque condimentum ut libero id blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.`,
+       eventtime: "7PM",
+       eventdate: "5/12/2021",
+        image:
+          "http://www.showticksecure.com/DBTR/VenueNet/_uploads/event_images/1_lrg_20211705131414_img.jpg",
+      },
+      {
+        id: "5",
+        name: "Summer concert",
+        address: "9/62 Argyle Street, South Windsor NSW 2756",
+        category: "Concerts",
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque est metus, gravida vel ex volutpat, posuere euismod tortor. Pellentesque tincidunt, mi ac varius blandit, quam orci dignissim risus, vitae rutrum orci urna ut neque.
+       Pellentesque condimentum ut libero id blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.`,
+       eventtime: "7PM",
+       eventdate: "5/12/2021",
+        image:
+          "http://www.showticksecure.com/DBTR/VenueNet/_uploads/event_images/1_lrg_20211705131414_img.jpg",
+      },
+      {
+        id: "6",
+        name: "Lions Club Richmond Markets",
+        address: "Hawkesbury Showground, Racecourse Road, Clarendon NSW 2756",
+        category: "Shopping Centres",
+        description: `Featuring crafts, local food and produce. Indoor and outdoor market. Enter via gate 2.`,
+       eventtime: "7.30am-1.30pm",
+       eventdate: "11/12/2021",
+       phoneno: "0415 647 226",
+        image:
+          "http://www.showticksecure.com/DBTR/VenueNet/_uploads/event_images/1_lrg_20211705131414_img.jpg",
+      },
+      {
+        id: "7",
+        name: "Windsor Mall Sunday Markets",
+        address: "Windsor Mall, George Street, Windsor NSW 2756",
+        category: "Shopping Centres",
+        description: `Featuring crafts, local food and produce. Indoor and outdoor market. Enter via gate 2.`,
+       eventtime: "7.30am-1.30pm",
+       eventdate: "11/12/2021",
+       phoneno: "0415 647 226",
+        image:
+          "http://www.showticksecure.com/DBTR/VenueNet/_uploads/event_images/1_lrg_20211705131414_img.jpg",
+      },
+      {
+        id: "8",
+        name: "Christmas in the Park at Bowen Mountain Park",
+        address: "Bowen Mountain Park, Lieutenant Bowen Road, Bowen Mountain NSW 2753",
+        category: "Concerts",
+        description: `Market Stalls & activities for kids`,
+       eventtime: "3pm-7pm",
+       eventdate: "18/12/2021",
+       phoneno: "",
+        image: require('../assets/images/Bowen-Mtn-Park-xmas.jpg'),
+      },
+      {
+        id: "9",
+        name: "Christmas in the Park at Bowen Mountain Park",
+        address: "Bowen Mountain Park, Lieutenant Bowen Road, Bowen Mountain NSW 2753",
+        category: "Concerts",
+        description: `Market Stalls & activities for kids`,
+       eventtime: "3pm-7pm",
+       eventdate: "18/12/2021",
+       phoneno: "",
+        image:
+          "http://www.showticksecure.com/DBTR/VenueNet/_uploads/event_images/1_lrg_20211705131414_img.jpg",
+      },
+      {
+        id: "10",
+        name: "Strawberry picking at Bilpin Fruit Bowl",
+        address: "2093 Bells Line of Road, Bilpin NSW 2758",
+        category: "Events",
+        description: `Pick your own strawberries, bok choy and assorted lettuce. 10am-4pm.Online bookings are essential to limit number of people. 
+        Admission fees apply and payment for produce taken home.`,
+       eventtime: "10am-4pm",
+       eventdate: "22/12/2021",
+       phoneno: "02 4567 1262",
+        image:
+          "http://www.showticksecure.com/DBTR/VenueNet/_uploads/event_images/1_lrg_20211705131414_img.jpg",
+      },
+      {
+        id: "11",
+        name: "Light up Windsor",
+        address: "Windsor Mall, George Street, Windsor NSW 2756",
+        category: "Events",
+        description: `The Christmas festival will kick off with the Street Fair on Wednesday, 22 December in Windsor Mall and George Street, Windsor from 5pm – 9pm.
+        Council has been able to include an exciting addition to the festival this year thanks to the Regional Tourism Bushfire Recovery Grant Program and to Destination Sydney Surrounds North. Light projections will feature on the front and back of the Old Hawkesbury Hospital as well as on the front of the Deerubbin Centre. The buildings will be lit up everynight from 18 December until Christmas Eve, 8-10pm.`,
+       eventtime: "10am-4pm",
+       eventdate: "22/12/2021",
+       phoneno: "02 4560 4444",
+        image:
+          "http://www.showticksecure.com/DBTR/VenueNet/_uploads/event_images/1_lrg_20211705131414_img.jpg",
+      },
+  ];
+  
+  
+  export default eventarray;
