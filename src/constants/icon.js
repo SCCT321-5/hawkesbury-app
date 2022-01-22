b70891a0e390e3066d937ef66904d612b7955f69
Illyphe Toumi-Cussinet <illyphetoumi-cussinet@outlook.com>
2022-01-22 19:37:05 +1100

    Hawkesbury App

--- src/constants/icon.js
+++
@@ -1,7 +0,0 @@
-const Marker = require("../assets/icon/baseline_place_black_48pt_3x.png")
-//const Markers = require("../assets/icon/outline_panorama_black_48pt_3x.png")
-
-export default {
-  Marker,
- // Markers
-}
